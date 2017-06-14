dd10467a2b26e8635195246e41768c6a214e5557
Bruno Silva <bsilva@cse.unl.edu>
2017-06-14 17:17:33 -0500

    getting correct result from fitbit

--- app/src/main/java/com/sbbi/obesityappv2/fragment/PreferencesFragment.java
+++ app/src/main/java/com/sbbi/obesityappv2/fragment/PreferencesFragment.java
@@ -23,6 +23,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.fitbit.api.ResourceLoadedHandler;
+import com.fitbit.api.services.CaloriesService;
+import com.fitbit.api.services.DeviceService;
+import com.fitbit.api.services.UserService;
 import com.fitbit.authentication.AuthenticationHandler;
 import com.fitbit.authentication.AuthenticationManager;
 import com.fitbit.authentication.AuthenticationResult;
@@ -31,6 +35,7 @@ import com.sbbi.obesityappv2.activity.LoginActivity;
 import com.sbbi.obesityappv2.activity.Test;
 import com.sbbi.obesityappv2.fitbit.FitbitAuthApplication;
 import com.sbbi.obesityappv2.fitbit.RootActivity;
+import com.sbbi.obesityappv2.fitbit.UserDataActivity;
 import com.sbbi.obesityappv2.helper.LogoutHelper;
 import com.sbbi.obesityappv2.request.FitbitApiHttp;
 
@@ -42,11 +47,7 @@ import java.net.URL;
 /**
  * Created by bsilva on 10/18/16.
  */
-public class PreferencesFragment extends Fragment {
-
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private Button btnLocation;
+public class PreferencesFragment extends Fragment implements ResourceLoadedHandler{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_preferences, container, false);
@@ -56,6 +57,10 @@ public class PreferencesFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 boolean loggedIn = AuthenticationManager.isLoggedIn();
+
+                Intent intent = new Intent(getActivity(), UserDataActivity.class);
+                startActivity(intent);
+
                 Log.i("LOGGED", loggedIn + "");
             }
         });
@@ -81,55 +86,6 @@ public class PreferencesFragment extends Fragment {
             }
         });
 
-        btnLocation = (Button) layout.findViewById(R.id.btnLocation);
-        btnLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO show location
-                locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-                locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        Log.i("LOCATION", "latitute: " + location.getLatitude() + "\t get longitude: " + location.getLongitude());
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(intent);
-                    }
-
-                };
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    requestPermissions(new  String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
-                    return;
-                }
-                else{
-                    configureButton();
-                }
-
-            }
-
-
-        });
-
         return layout;
     }
 
@@ -139,23 +95,13 @@ public class PreferencesFragment extends Fragment {
         startActivity(new Intent(getActivity(), LoginActivity.class));
     }
 
-    private void configureButton() {
-        btnLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-            }
-        });
+    @Override
+    public void onResourceLoaded(Object resource) {
 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 10:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    configureButton();
-                return;
-        }
+    public void onResourceLoadError(String errorMessage) {
+
     }
 }
--- fitbitapi/src/main/java/com/fitbit/api/services/CaloriesService.java
+++ fitbitapi/src/main/java/com/fitbit/api/services/CaloriesService.java
@@ -8,9 +8,14 @@ import com.fitbit.api.MissingScopesException;
 import com.fitbit.api.ResourceLoadedHandler;
 import com.fitbit.api.ResourceLoader;
 import com.fitbit.api.TokenExpiredException;
+import com.fitbit.api.models.ActivitiesCalories;
 import com.fitbit.api.models.CaloriesContainer;
+import com.fitbit.api.models.User;
+import com.fitbit.api.models.UserContainer;
 import com.fitbit.authentication.AuthenticationManager;
 import com.fitbit.authentication.Scope;
+
+import java.util.List;
 
 /**
  * Created by bsilva on 2/23/17.
@@ -21,8 +26,24 @@ public class CaloriesService {
     private final static String CALORIES_URL = "https://api.fitbit.com/1/user/-/activities/calories/date/2016-05-18/1d/15min.json";
     private static final ResourceLoader<CaloriesContainer> USER_CALORIES_LOADER = new ResourceLoader<>(CALORIES_URL, CaloriesContainer.class);
 
-    public static void getUserCaloriesToday(Activity activityContext, @NonNull final ResourceLoadedHandler<CaloriesContainer> handler) throws MissingScopesException, TokenExpiredException {
+    public interface CalorieHandler {
+        void onCalorieLoaded(List<ActivitiesCalories> container);
+
+        void onErrorLoadingCalorie(String errorMessage);
+    }
+
+    public static void getUserCaloriesToday(Activity activityContext, @NonNull final CalorieHandler handler) throws MissingScopesException, TokenExpiredException {
         APIUtils.validateToken(activityContext, AuthenticationManager.getCurrentAccessToken(), Scope.activity);
-        USER_CALORIES_LOADER.loadResource(activityContext, handler);
+        USER_CALORIES_LOADER.loadResource(activityContext, new ResourceLoadedHandler<CaloriesContainer>() {
+            @Override
+            public void onResourceLoaded(CaloriesContainer resource) {
+                handler.onCalorieLoaded(resource.getListActivitiesCalories());
+            }
+
+            @Override
+            public void onResourceLoadError(String errorMessage) {
+
+            }
+        });
     }
 }
